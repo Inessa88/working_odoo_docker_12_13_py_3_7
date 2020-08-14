5a5e4ff045cc6d13bd7907c79315032643b01fff
Vitaly Kazimirov <v.kazimirov@invento.by>
2020-08-14 14:56:35 +0300

    Learning Java Script in Odoo: added button with action (alert) in js

---
+++ src/pos_action_button/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Dashboard',
+    'version': '12.0.1.0',
+    'summary': 'Dashboard button in POS screen',
+    'category': 'Point Of Sales',
+    'author': 'Vitaly',
+    'maintainer': 'Vitaly',
+    'company': 'Vitaly',
+    'website': 'https://www.google.com',
+    'depends': ['base', 'point_of_sale'],
+    'qweb': ['static/src/view/action_button.xml'],
+    'data': ['views/view.xml'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
