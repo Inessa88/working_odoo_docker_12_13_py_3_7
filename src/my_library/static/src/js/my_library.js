be32abc888a4e929fb03e1269c89fb47f25e963a
Vitaly Kazimirov <v.kazimirov@invento.by>
2020-08-16 18:42:10 +0300

    Added controller for /books route, switched off alert "Hello world"

--- src/my_library/static/src/js/my_library.js
+++ src/my_library/static/src/js/my_library.js
@@ -1,6 +1,6 @@
 odoo.define('my_library', function (require) {
     var core = require('web.core');
-    alert(core._t('Hello world'));
+    // alert(core._t('Hello world'));
     return {
         // if you created functionality to export, add it here
     }
