e31d8d8f7f6544cc389fa1691e783261d2b11543
Inessa <petrova.inessa1906@yandex.ru>
2020-12-09 18:30:31 +0300

    Done until page 143

--- src/my_library/__manifest__.py
+++ src/my_library/__manifest__.py
@@ -1,24 +1,16 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "My Library",  # Module title
-    'summary': "Manage books easily",  # Module subtitle phrase
-    'description': """Long description""",  # You can also rst format
-    'author': "Parth Gajjar",
+    'name': "My library",
+    'summary': "Manage books easily",
+    'description': """Long description""",
+    'author': "Your name",
     'website': "http://www.example.com",
     'category': 'Uncategorized',
     'version': '12.0.1',
-    'depends': ['base'],
-    # This data files will be loaded at the installation (commented becaues file is not added in this example)
+    'depends': ['base', 'decimal_precision'],
     'data': [
-        'security/groups.xml',
-        'security/ir.model.access.csv',
-        'views/library_book.xml'
+            'security/groups.xml',
+            'security/ir.model.access.csv',
+            'views/library_book.xml',
     ],
-    # This demo data files will be loaded if db initialize with demo data (commented becaues file is not added in this example)
-    # 'demo': [
-    #     'demo.xml'
-    # ],
-    'installable': True,
-    'application': True,
-
 }
+
--- src/my_library/models/library_book.py
+++ src/my_library/models/library_book.py
@@ -1,38 +1,48 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields, api
+from odoo import models, fields
+from odoo.addons import decimal_precision as dp
 
 
 class LibraryBook(models.Model):
     _name = 'library.book'
     _description = 'Library Book'
-
+    _order = 'date_release desc, name'
+    _rec_name = 'short_name'
     name = fields.Char('Title', required=True)
-    isbn = fields.Char('ISBN')
-    active = fields.Boolean('Active?', default=True)
-    date_published = fields.Date()
-    image = fields.Binary('Cover')
-    publisher_id = fields.Many2one('res.partner', string='Publisher')
+    short_name = fields.Char('Short Title', required=True)
+    notes = fields.Text('Internal Notes')
+    state = fields.Selection(
+        [('draft', 'Not Available'),
+        ('available', 'Available'),
+        ('lost', 'Lost')],
+        'State')
+    description = fields.Html('Description')
+    cover = fields.Binary('Book Cover')
+    out_of_print = fields.Boolean('Out of Print?')
+    date_release = fields.Date('Release Date')
+    date_updated = fields.Datetime('Last Updated')
+    pages = fields.Integer('Number of Pages')
+    reader_rating = fields.Float(
+        'Reader Average Rating',
+        digits=(14, 4),  # Optional precision (total, decimals)
+    )
     author_ids = fields.Many2many('res.partner', string='Authors')
+    cost_price = fields.Float('Book Cost', dp.get_precision('Book Price'))
+    currency_id = fields.Many2one(
+        'res.currency', string='Currency')
+    retail_price = fields.Monetary(
+        'Retail Price',
+        # optional: currency_field='currency_id',
+        )
+    publisher_id = fields.Many2one('res.partner', string='Publisher')
 
-    @api.multi
-    def _check_isbn(self):
-        """Check one Book's ISBN"""
-        self.ensure_one()
-        digits = [int(x) for x in self.isbn if x.isdigit()]
-        if len(digits) == 13:
-            ponderators = [1, 3] * 6
-            total = sum(a * b for a, b in zip(digits[:12], ponderators))
-            remain = total % 10
-            check = 10 - remain if remain != 0 else 0
-            return digits[-1] == check
 
-    @api.multi
-    def button_check_isbn(self):
-        for book in self:
-            if not book.isbn:
-                raise Warning(
-                    'Please provide an ISBN13 for %s' % book.name)
-            if book.isbn and not book._check_isbn():
-                raise Warning(
-                    '%s is an invalid ISBN' % book.isbn)
-        return True
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    published_book_ids = fields.One2many(
+        'library.book', 'publisher_id',
+        string='Published Books')
+    authored_book_ids = fields.Many2many(
+        'library.book',
+        string='Authored Books',
+        # relation='library_book_res_partner_rel'
+)
